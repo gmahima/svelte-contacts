3fbad5cc88b9ce8d2295ac12888d2f9d5a4436fb
gmahima <mahimagangavarapu@gmail.com>
2020-08-11 20:33:51 +0530

    set up tailwind

---
+++ src/App.svelte
@@ -0,0 +1,9 @@
+<script>
+	export let name;
+</script>
+
+
+<div class="bg-gray-100 rounded-lg text-center h-full min-h-screen flex flex-col ">
+	<h1 class="bg-white p-4 ">My contacts</h1>
+</div>
+
