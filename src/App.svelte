406bf5bb2f540ab9c748a22a05843d075f108b2f
gmahima <mahimagangavarapu@gmail.com>
2020-08-19 11:18:56 +0530

    add routing

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,8 @@
 	import contacts from './stores/contacts'
 	import ContactList from './ContactList.svelte'
 	import AddContactForm from './AddContactForm.svelte'
-	
+	import { Router, Route, Link } from "svelte-routing";
+	export let url = "";
 
 // contacts.subs returns a function to run before unmount
 	// let contacts;
@@ -11,15 +12,21 @@
 	// });
 </script>
 
-
 <div class="bg-gray-100 rounded-lg text-center h-full min-h-screen flex flex-col ">
-	<h1 class="bg-teal-400 p-4 sm:text-4xl text-white capitalize">My contacts</h1>
-	<div class="flex justify-around border border-red-700 my-auto">
-		<ContactList />
-		<AddContactForm />
-	</div>
-	
-	
+	<Router url="{url}">
+		<div class="bg-teal-400 flex justify-between">
+			<h1 class=" p-4 sm:text-4xl text-white capitalize">My contacts</h1>
+			<nav>
+				<Link to="/">Home</Link>
+				<Link to="add-contact">Add Contact</Link>
+			</nav>
+		</div>
+		
+			<Route path="add-contact" component="{AddContactForm}" />
+			<Route path="/" component="{ContactList}" />
+		
+ 
+	</Router>
 	
 </div>
 
